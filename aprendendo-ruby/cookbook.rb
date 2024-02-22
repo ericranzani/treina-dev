9969a4ba8a157fc6a8e0219ee638b9eb51ec0a6b
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-02-22 12:51:14 -0300

    Aula de estrutura condicional

--- aprendendo-ruby/cookbook.rb
+++ aprendendo-ruby/cookbook.rb
@@ -2,14 +2,33 @@ puts "Bem-vindo ao Cookbook, sua rede social de receitas"
 
 receitas = []
 
-while(1 == 1) do
-    puts "Digite o nome da receita: "
-    receita = gets.chomp()
+puts "[1] Cadastrar uma receita"
+puts "[2] Ver todas as receitas"
+puts "[3] Sair"
 
-    receitas << receita
+print "Escolha uma opção: "
+opcao = gets.to_i()
 
-    puts "Receita #{receita} cadastrada com sucesso!"
-    puts
-    puts "=========== Receitas cadastradas ==========="
-    puts receitas # no ruby quando voce pede para pintar uma mensagem de uma lista, o proprio ruby ja identifica q é cada elemento da lista
+while(opcao != 3) do
+    if (opcao == 1)
+        puts "Digite o nome da receita: "
+        receita = gets.chomp()
+        receitas << receita
+        puts "Receita #{receita} cadastrada com sucesso!"
+        puts
+    elsif (opcao == 2)
+        puts "=========== Receitas cadastradas ==========="
+        puts receitas # no ruby quando voce pede para pintar uma mensagem de uma lista, o proprio ruby ja identifica q é cada elemento da lista
+    else
+        puts "Opção invalida, digite um valor valido"
+    end
+
+puts "[1] Cadastrar uma receita"
+puts "[2] Ver todas as receitas"
+puts "[3] Sair"
+
+print "Escolha uma opção: "
+opcao = gets.to_i()
 end
+
+puts "Obrigado por utilizar o Cookbook, até logo"
