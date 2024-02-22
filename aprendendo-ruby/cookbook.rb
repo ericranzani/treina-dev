f5dab56513341f07fd06a5933d06dc04802fb0fd
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-02-22 13:36:44 -0300

    ccraição dos metodos

--- aprendendo-ruby/cookbook.rb
+++ aprendendo-ruby/cookbook.rb
@@ -1,39 +1,49 @@
-puts "Bem-vindo ao Cookbook, sua rede social de receitas"
+def ben_vindo()
+    puts "Bem-vindo ao Cookbook, sua rede social de receitas"
+end
+
+def menu()
+    puts "[1] Cadastrar uma receita"
+    puts "[2] Ver todas as receitas"
+    puts "[3] Sair"
+
+    print "Escolha uma opção: "
+    return gets.to_i()
+end
+
+def inserir_receita()
+    puts "Digite o nome da receita: "
+    receita = gets.chomp()
+    puts "Digite o ntipo da receita: "
+    tipo = gets.chomp()
+    
+    puts "Receita #{receita} cadastrada com sucesso!"
+    puts
+    return {nome: receita, tipo: tipo}
+end    
+
+def imprimir_receitas(receitas)
+    puts "=========== Receitas cadastradas ==========="
+    receitas.each do |receita|
+        puts "#{receita[:nome]} - #{receita[:tipo]}"
+    end  
+end
+
+ben_vindo()
 
 receitas = []
 
-puts "[1] Cadastrar uma receita"
-puts "[2] Ver todas as receitas"
-puts "[3] Sair"
-
-print "Escolha uma opção: "
-opcao = gets.to_i()
+opcao = menu()
 
 while(opcao != 3) do
     if (opcao == 1)
-        puts "Digite o nome da receita: "
-        receita = gets.chomp()
-        puts "Digite o ntipo da receita: "
-        tipo = gets.chomp()
-        receitas << {nome: receita, tipo: tipo}
-
-        puts "Receita #{receita} cadastrada com sucesso!"
-        puts
+        receitas << inserir_receita()
     elsif (opcao == 2)
-        puts "=========== Receitas cadastradas ==========="
-        receitas.each do |receita|
-            puts "#{receita[:nome]} - #{receita[:tipo]}"
-        end    
+        imprimir_receitas(receitas)
     else
         puts "Opção invalida, digite um valor valido"
     end
-
-puts "[1] Cadastrar uma receita"
-puts "[2] Ver todas as receitas"
-puts "[3] Sair"
-
-print "Escolha uma opção: "
-opcao = gets.to_i()
+opcao = menu()
 end
 
 puts "Obrigado por utilizar o Cookbook, até logo"
