b5a9dd68e19af959f839839f7fd3ae4454daa200
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-02-22 12:22:49 -0300

    continuando com coneceitos básicos na qual foi listas e estrutura de repetição

--- aprendendo-ruby/cookbook.rb
+++ aprendendo-ruby/cookbook.rb
@@ -1,6 +1,15 @@
 puts "Bem-vindo ao Cookbook, sua rede social de receitas"
 
-puts "Digite o nome da receita: "
-gets
+receitas = []
 
-puts "Receita cadastrada com sucesso!"
+while(1 == 1) do
+    puts "Digite o nome da receita: "
+    receita = gets.chomp()
+
+    receitas << receita
+
+    puts "Receita #{receita} cadastrada com sucesso!"
+    puts
+    puts "=========== Receitas cadastradas ==========="
+    puts receitas # no ruby quando voce pede para pintar uma mensagem de uma lista, o proprio ruby ja identifica q é cada elemento da lista
+end
