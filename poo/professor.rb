0a702865f01d2edc58cddc5f48c3b4bb4f20f4db
Éric Ranzani <79282304+ericranzani@users.noreply.github.com>
2024-02-23 11:16:26 -0300

    atualizando a classe professor com o metodo de ferias

--- poo/professor.rb
+++ poo/professor.rb
@@ -1,7 +1,14 @@
 class Aluno
-    attr_accessor :nome, :codigoF
-    def initialize(nome, codigoF)
+    attr_reader :ferias
+    attr_accessor :nome, :codigoF, :disciplina
+    def initialize(nome, codigoF, disciplina)
         @nome = nome
         @codigoF = codigoF
+        @disciplina = disciplina
+        @ferias = false
+    end
+
+    def inicia_ferias()
+        @ferias = true
     end
 end
